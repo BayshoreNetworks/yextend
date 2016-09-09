9781a3da341c20c9add46591e38e1ab08a268e39
Dre <andres@neurofuzzsecurity.com>
2016-09-08 20:34:57 -0400

    pdf_parser - refactor necessary for support of upcoming search for embedded files

--- libs/pdf_parser.cpp
+++ libs/pdf_parser.cpp
@@ -11,10 +11,72 @@
 static const char *tmp_path = "/tmp/";
 static const char *pdf_to_text = "pdftotext";
 
-PDFParser::PDFParser() {
+static std::string get_stdout_cmd(std::string lcmd) {
+
+	std::string data;
+	FILE *stream;
+	const int max_buffer = 1024;
+	char buffer[max_buffer];
+	lcmd.append(" 2>&1");
+
+	stream = popen(lcmd.c_str(), "r");
+	if (stream) {
+		while (!feof(stream))
+			if (fgets(buffer, max_buffer, stream) != NULL)
+				data.append(buffer);
+		pclose(stream);
+	}
+	return data;
+}
+
+PDFParser::PDFParser(const uint8_t *buffer, size_t buffer_length) {
+	
+    char *input_filename = NULL;
+    char *input_filepath = NULL;
+    char *output_filename = NULL;
+    char *output_filepath = NULL;
+    
+    size_t tmp_path_len = strlen(tmp_path);
+    size_t uuid_len = 32;
+    uuid_t id;
+    
+    //////////////////////////////////////////////////////////
+    uuid_generate(id);
+    input_filename = new char[uuid_len + tmp_path_len];
+    uuid_unparse(id, input_filename);
+    input_filepath = new char[tmp_path_len + strlen(input_filename)];
+    strcpy(input_filepath, tmp_path);
+    strcat(input_filepath, input_filename);
+    // write original buffer to file
+    std::ofstream fp;
+    fp.open(input_filepath, std::ios::out | std::ios::binary );
+    fp.write((char*)buffer, buffer_length);
+    fp.close();
+    //////////////////////////////////////////////////////////
+    uuid_generate(id);
+    output_filename = new char[uuid_len + tmp_path_len];
+    uuid_unparse(id, output_filename);
+    output_filepath = new char[tmp_path_len + strlen(output_filename)];
+    strcpy(output_filepath, tmp_path);
+    strcat(output_filepath, output_filename);
+    
+	// set class variables
+	stored_file_name = std::string(input_filepath, strlen(input_filepath));
+	buf_len = buffer_length;
+	extracted_file_name = std::string(output_filepath, strlen(output_filepath));
+	
+	// clean up local vars
+    delete input_filename;
+    delete input_filepath;
+    delete output_filename;
+    delete output_filepath;
+    
 }
 
 PDFParser::~PDFParser() {
+	// should we shred here instead?
+	remove(stored_file_name.c_str());
+	remove(extracted_file_name.c_str());
 }
 
 std::string PDFParser::ExtractText( const char* filepath  )
@@ -39,10 +101,6 @@ std::string PDFParser::exc_ExtractText( const char* filepath  )
         strcpy(output_filepath, tmp_path);
         strcat(output_filepath, output_filename);
 
-        /*
-         * pdftotext = the first 9
-         * 9 + 1 + strlen(filepath) + 1 + strlen(output_filepath) + 1
-         */
         cmd = new char[12 + strlen(filepath) + strlen(output_filepath)];
         strcpy(cmd, pdf_to_text);
         strcat(cmd, " ");
@@ -73,81 +131,45 @@ std::string PDFParser::exc_ExtractText( const char* filepath  )
 	}
 }
 
-std::string PDFParser::extract_text_buffer(const uint8_t *buffer, size_t buffer_length)
+std::string PDFParser::extract_text_buffer()
 {
-    return exc_extract_text_buffer (buffer, buffer_length);
+    return exc_extract_text_buffer();
 }
 
-std::string PDFParser::exc_extract_text_buffer(const uint8_t *buffer, size_t buffer_length)
+std::string PDFParser::exc_extract_text_buffer()
 {
-    char *input_filename = NULL;
-    char *input_filepath = NULL;
-    char *output_filename = NULL;
-    char *output_filepath = NULL;
-    char *cmd = NULL;
-    
+    char *cmd = NULL;   
     try {
-        uuid_t id;
-        uuid_generate(id);
-    	// 36 + 1
-        input_filename = new char[37];
-        uuid_unparse(id, input_filename);
-        // 5+strlen(input_filename)+1
-        input_filepath = new char[6 + strlen(input_filename)];
-        strcpy(input_filepath, tmp_path);
-        strcat(input_filepath, input_filename);
+    	
+        char * cmd = new char[12 + stored_file_name.size() + extracted_file_name.size()];
         
-        std::ofstream fp;
-        fp.open(input_filepath, std::ios::out | std::ios::binary );
-        fp.write((char*)buffer, buffer_length);
-        fp.close();
-
-        // 36 + 1
-        char *output_filename = new char[37];
-        uuid_generate(id);
-        uuid_unparse(id, output_filename);
-        char *output_filepath = new char[5+strlen(output_filename)+1];
-        strcpy(output_filepath, tmp_path);
-        strcat(output_filepath, output_filename);
-
-        /*
-         * pdftotext = the first 9
-         * 9 + 1 + strlen(input_filepath) + 1 + strlen(output_filepath) + 1
-         */
-        char * cmd = new char[12 + strlen(input_filepath) + strlen(output_filepath)];
         strcpy(cmd, pdf_to_text);
         strcat(cmd, " ");
-        strcat(cmd, input_filepath);        
-        strcat(cmd, " ");        
-        strcat(cmd, output_filepath);        
+        strcat(cmd, stored_file_name.c_str());
+        strcat(cmd, " ");
+        strcat(cmd, extracted_file_name.c_str());
         system(cmd);
 
-        remove(input_filepath);
-
-        std::ifstream ifs(output_filepath);
+        std::ifstream ifs(extracted_file_name.c_str());
         std::string content( (std::istreambuf_iterator<char>(ifs) ),
-            (std::istreambuf_iterator<char>()) );
-        remove(output_filepath);
-    
-        delete input_filename;
-        delete input_filepath;
-        delete output_filename;
-        delete output_filepath;
-        delete cmd;
-        
+        		(std::istreambuf_iterator<char>()) );
+
+        delete cmd;        
         return content;
     } catch (std::exception e) {
-        if(input_filename) {delete input_filename;}
-        if(input_filepath) {delete input_filepath;}
-        if(output_filename) {delete output_filename;}
-        if(output_filepath) {delete output_filepath;}
+
         if(cmd) {delete cmd;}
-        
-        remove(input_filepath);
-        remove(output_filepath);
 
         syslog (LOG_INFO|LOG_LOCAL6, "PDFParser encountered fatal error");
         return "";
     }
+}
+
+
+int PDFParser::has_embedded_files(const uint8_t *) {
+	
+	std::cout << get_stdout_cmd("pdfdetach -list test_files/lipsum.txt.pdf") << std::endl;
+	return 1;
+	
 }
 
--- libs/pdf_parser.h
+++ libs/pdf_parser.h
@@ -12,19 +12,21 @@
 
 class PDFParser {
 public:
-	PDFParser();
+	PDFParser(const uint8_t *, size_t);
 	virtual ~PDFParser();
-	std::string  ExtractText( const char* filepath  );
+	std::string ExtractText( const char* filepath  );
 	
 public:
-	std::string extract_text_buffer(const uint8_t *, size_t);
-	//char *output_buffer;
-	//std::string output_buffer;
+	std::string extract_text_buffer();
+	int has_embedded_files(const uint8_t *);
 
 private:
-	std::string  exc_ExtractText( const char* filepath );
-	std::string exc_extract_text_buffer(const uint8_t *, size_t);
+	std::string exc_ExtractText( const char* filepath );
+	std::string exc_extract_text_buffer();
 
+	std::string stored_file_name;
+	size_t buf_len;
+	std::string extracted_file_name;
 };
 
 #endif /* PDFPARSER_H_ */
